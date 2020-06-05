260a10aaec88896788b54631781c35d8b946d666
RickyC0626 <rickychon99@gmail.com>
2020-06-04 20:43:42 -0400

    Complete SearchBar template from zip-search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,38 @@ export default class SearchBar extends Component
     constructor(props)
     {
         super(props);
+        this.state =
+        {
+            prevSearch: "Try Brooklyn",
+            hasSearched: false
+        }
+    }
+
+    handleSubmit = (event) =>
+    {
+        // Prevent browser reload/refresh
+        event.preventDefault();
+
+        this.setState({ prevSearch: event.target.searchbar.value, hasSearched: true });
+        event.target.reset();
     }
 
     render()
     {
         return (
+            this.state.hasSearched ?
             <section>
-                
+                <form onSubmit={this.handleSubmit}>
+                    <div>City</div>
+                    <input type="text" name="searchbar" id="searchbar" placeholder={this.state.prevSearch} />
+                </form>
+            </section>
+            :
+            <section>
+                <form onSubmit={this.handleSubmit}>
+                    <div>City</div>
+                    <input type="text" name="searchbar" id="searchbar" placeholder={this.state.prevSearch} />
+                </form>
             </section>
         );
     }
