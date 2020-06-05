b433e44030e316a5336140d1a4a3c42cb2bed018
RickyC0626 <rickychon99@gmail.com>
2020-06-04 20:50:20 -0400

    Add template for CityCard component

---
+++ src/components/CityCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CityCard extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+
+    }
+}
+
+CityCard.propTypes =
+{
+    
+}
