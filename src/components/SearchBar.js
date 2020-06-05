a9ac2f612cbdd425b06e035776b2c2f9e53f3921
RickyC0626 <rickychon99@gmail.com>
2020-06-04 20:40:12 -0400

    Add SearchBar template

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <section>
+                
+            </section>
+        );
+    }
+}
